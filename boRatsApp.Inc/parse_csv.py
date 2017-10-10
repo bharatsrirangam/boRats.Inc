80ab22126adf0885e968bf61fb7a3c5e9f2c3ebc
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-10-09 22:39:50 -0400

    added parallelization, continues from where workers stopped last

--- boRatsApp.Inc/parse_csv.py
+++ boRatsApp.Inc/parse_csv.py
@@ -1,6 +1,8 @@
 import sys
+import threading
 
-def main(argv):
+def worker(f,t):
+
     from firebase import firebase
 
     file = open("C:\Users\Robert\Downloads\Rat_Sightings.csv","r")
@@ -8,14 +10,27 @@ def main(argv):
 
     url = 'https://bhoratsdb.firebaseio.com/'
     firebase = firebase.FirebaseApplication(url, None)
-    for line in lines[1:2]:
+    if(f>len(lines)):
+        return
+    if(t>len(lines)):
+        t=len(lines)
+    for line in lines[f:t]:
         if(line):
             line = line.split(",")
-
             data = {"borough":line[23],"city":line[16],"created":line[1],"incident_address":line[9],"incident_zip":line[8],"lat":line[49],"location_type":line[7],"lon":line[50]}
-            result = firebase.patch('/RatSightings/'+str(line[0]),data)
-            print result
+            result = firebase.patch('/RatSightingsTEST/'+str(line[0]),data)
     file.close()
+    return
+
+
+
+def main(argv):
+    threads = []
+    for i in range(10):
+        t = threading.Thread(target=worker,args=(1691+10000*i,1691+10000*(i+1))
+        #t = threading.Thread(target=worker,args=(i,i+1)) testing line
+        threads.append(t)
+        t.start()
 
 if __name__ == '__main__':
   main(sys.argv)
