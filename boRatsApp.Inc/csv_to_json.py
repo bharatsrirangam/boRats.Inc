c069b6adadf440bf9d4667505c16d226656956f1
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-10-11 22:09:25 -0400

    working parsing code to convert CSV data to json

---
+++ boRatsApp.Inc/csv_to_json.py
@@ -0,0 +1,14 @@
+
+file = open("C:\Users\Robert\Downloads\Rat_Sightings.csv","r")
+file_json = open("C:\Users\Robert\Downloads\Rat_Sightings.json","w")
+lines = file.readlines()
+import json
+data = {}
+for line in lines[1:]:
+    if(line):
+        line = line.split(",")
+        data[line[0]] = {"borough":line[23],"city":line[16],"created":line[1],"incident_address":line[9],"incident_zip":line[8],"lat":line[49],"location_type":line[7],"lon":line[50]}
+file_json.write(json.dumps({"RatSightings":data}))
+file.close()
+
+
