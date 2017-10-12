bac8b86885df3ba7a600f8361561a5b7aed60222
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-10-11 22:27:04 -0400

    added ukey as another field in db

--- boRatsApp.Inc/csv_to_json.py
+++ boRatsApp.Inc/csv_to_json.py
@@ -7,7 +7,7 @@ data = {}
 for line in lines[1:]:
     if(line):
         line = line.split(",")
-        data[line[0]] = {"borough":line[23],"city":line[16],"created":line[1],"incident_address":line[9],"incident_zip":line[8],"lat":line[49],"location_type":line[7],"lon":line[50]}
+        data[line[0]] = {"key":line[0],"borough":line[23],"city":line[16],"created":line[1],"incident_address":line[9],"incident_zip":line[8],"lat":line[49],"location_type":line[7],"lon":line[50]}
 file_json.write(json.dumps({"RatSightings":data}))
 file.close()
 
