aa121b1b72cc18ba8d06afd868aabc63cf9a22f9
robert_kuramshin <robert.kuramshin98@gmail.com>
2017-11-01 18:53:11 -0400

    parse date as string instead of int

--- boRatsApp.Inc/csv_to_json.py
+++ boRatsApp.Inc/csv_to_json.py
@@ -11,7 +11,6 @@ for line in lines[1:]:
         address = line[1]
         address=re.split('/| |:',address)
         address = address[2]+address[0]+address[1]+address[3]+address[4]+address[5]
-        address = int(address)
         data[line[0]] = {"key":line[0],"borough":line[23],"city":line[16],"created":line[1],"incident_address":line[9],"incident_zip":line[8],"lat":line[49],"location_type":line[7],"lon":line[50],"date_num":address}
 file_json.write(json.dumps({"RatSightings":data}))
 file.close()
