38a023aefb7f0e10a72ea975cfa4b6d0d3c7d150
Jarrett Schultz <jschultz38@gatech.edu>
2017-12-05 10:26:07 -0500

    Tests added

--- boRatsApp.Inc/app/src/test/java/com/boratsinc/ExampleUnitTest.java
+++ boRatsApp.Inc/app/src/test/java/com/boratsinc/ExampleUnitTest.java
@@ -18,10 +18,6 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
-    @Test
     /*
      * Written by Jarrett Schultz to test addSighting(RatSighting r) et al. in
      * Model class
